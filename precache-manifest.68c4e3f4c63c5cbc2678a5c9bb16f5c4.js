ce0e8ae8c798c9979258dff5bce5d991d4777a86
amtkrc <ahmetki@uia.no>
2025-09-25 13:38:34 +0200

    Updates

---
+++ precache-manifest.68c4e3f4c63c5cbc2678a5c9bb16f5c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6606c367a8589416e4cc772dc07c95ee",
+    "url": "/Group-20-Portfolio/index.html"
+  },
+  {
+    "revision": "b497b22a3a51256499d4",
+    "url": "/Group-20-Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "bc99299df75a8f233d18",
+    "url": "/Group-20-Portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "b497b22a3a51256499d4",
+    "url": "/Group-20-Portfolio/static/js/2.2166028c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Group-20-Portfolio/static/js/2.2166028c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc99299df75a8f233d18",
+    "url": "/Group-20-Portfolio/static/js/main.49233740.chunk.js"
+  },
+  {
+    "revision": "f875350e576cf4758856",
+    "url": "/Group-20-Portfolio/static/js/runtime-main.e07520c6.js"
+  }
+]);
